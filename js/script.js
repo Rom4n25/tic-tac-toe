87b0c3bc3c719ad4350b61c85eed1b6bb84101ca
roman <mromanek1802@gmail.com>
2022-05-31 20:19:50 +0200

    Add player details div

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const gameBoard = (() => {
+    board = ["X","X","X","X","X","O","O","O","O"];
+
+    return {board};
+})();
+
+const displayController = (() => {
+
+
+})();
+
+
+const Player = () =>{
+    const name = document.getElementById("player_name");
+    const marker = document.querySelector(".marker");
+}

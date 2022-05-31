cdd5613290d6d35766cde6a5903b69e13058d598
roman <mromanek1802@gmail.com>
2022-05-31 21:25:00 +0200

    Add eventListener to gameboard boxes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
+const displayController = (() => {
+    
+    return {};
+})();
+
 const gameBoard = (() => {
-    board = ["X","X","X","X","X","O","O","O","O"];
+    let board = ["X","X","X","X","X","O","O","O","O"];
+    const boxOne = document.getElementById("1");
+    const boxTwo = document.getElementById("2");
+    const boxThree = document.getElementById("3");
+    const boxFour = document.getElementById("4");
+    const boxFive = document.getElementById("5");
+    const boxSix = document.getElementById("6");
+    const boxSeven = document.getElementById("7");
+    const boxEight = document.getElementById("8");
+    const boxNine = document.getElementById("9");
+    const boxes = [boxOne,boxTwo,boxThree,boxFour,boxFive,boxSix,boxSeven,boxEight,boxNine];
+
+    boxes.forEach(box => box.addEventListener("click", () => console.log(box)));
 
     return {board};
 })();
-
-const displayController = (() => {
-
-
-})();
-
 
 const Player = () =>{
     const name = document.getElementById("player_name");

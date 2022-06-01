08883a7b77aa835259d690c4157e85b53e79511a
roman <mromanek1802@gmail.com>
2022-06-01 22:47:38 +0200

    Refactor, add computer random logic

--- js/script.js
+++ js/script.js
@@ -1,38 +1,64 @@
-const displayController = (() => {
-    const start = () =>{
-        const player = Player();
-        console.log("Player name:" + player.name);
-        console.log("Player marker:" + player.marker);
-        gameBoard.board = [];
-       
+const displayController = (() => {    
+
+    const computerPlaceMarker = (boxId,marker) =>{
+        const box = document.getElementById(boxId.toString());
+        box.textContent=marker;
     }
-    
-    
-    const selectBox = (box) =>{
-        console.log(box.id);
-    
-      
+
+    const playerPlaceMarker = (box,marker) =>{
+        box.textContent = marker;
     };
     
-    return {selectBox,start};
+    return {playerPlaceMarker,computerPlaceMarker};
 })();
 
-const gameBoard = (() => {
-    
+const game = (() => {
+
+    let board = [];
+
+    function computerMove(marker){
+
+      while(true) {
+        let randomNumber = Math.round(Math.random()*9);
+        if(board[randomNumber]==null){
+            board[randomNumber] = marker;
+            displayController.computerPlaceMarker(randomNumber,marker);
+            break;
+        }else{
+            continue;
+        }
+      } 
+    }
+
+    function start(){
+        const player = Player();
+        let computerMarker = "";
+
+        if(player.marker=="X"){
+            computerMarker = "O";
+        }else{
+            computerMarker = "X"
+        };
+
+        markerBoxes.forEach(box => box.addEventListener("click", () => {
+            displayController.playerPlaceMarker(box,player.marker);
+            board[box.id] = player.marker;
+            computerMove(computerMarker);
+            console.log(board);
+    }));
+    } 
+
     const startBtn = document.getElementById("start");
-    const boxes = document.querySelectorAll(".marker-box");
+    const markerBoxes = document.querySelectorAll(".marker-box");
 
-    let board = ["X","X","X","X","X","O","O","O","O"];
+    startBtn.addEventListener("click",start);
     
-    startBtn.addEventListener("click",displayController.start);
-    
-    boxes.forEach(box => box.addEventListener("click", () => {displayController.selectBox(box)}));
-
-    return {board};
+    return {markerBoxes};
 })();
 
 const Player = () =>{
-    const name = document.getElementById("player_name").value;
     const marker = document.querySelector('input[name="marker"]:checked').value;
-    return {name,marker};
+    const name = document.getElementById("player_name").value;
+
+    return{name,marker};
 }
